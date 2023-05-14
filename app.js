f68da6814a84a5673fc1b1fd14073ffae460516b
Sudeep4686 <sudeephipparge@gmail.com>
2023-05-15 00:05:50 +0530

    fullstackBookingAppointmentApp

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const controller = require('./controller');
+
+const sequelize = require('./util/database');
+
+const User = require('./models/User');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync()
+    .then(()=>{
+        console.log('details synchronised with database');
+    })
+    .catch((error)=>{
+        console.log("failed to sync the data with the database");
+    });
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'views', 'form.html'));
+});
+
+
+app.post('/submit-form', controller.insertData);
+
+app.get('/users',async (req,res)=>{
+    try{
+        const users = await User.findAll();
+        console.log(users, 'data is found');
+        res.status(200).send(users);
+    }catch(err) {
+        console.log(err,"Error is found here");
+        res.status(500).send('Something went Wrong');
+    };
+});
+
+app.delete('/User/delete-user/:id' , async(req,res)=>{
+    try{
+    const userId = req.params.id;
+    await User.destroy({where: {id:userId}});
+    res.json({message: "User deleted successfully"});
+    } catch(err){
+        res.sendStatus(500).json({
+            error:err,
+        });
+    }
+})
+
+// app.update('/User/update-user/:id', async(req,res)=>{
+//     try{
+//         const editMode = req.query.edit;
+//         if(!editMode) {
+//             return res.redirect('/');
+//         }
+//         const userId = req.params.id;
+//         users.findById(userId)
+//         .then(user =>{
+//             if (!user) {
+//                 return res.redirect('/');
+//             }
+//         })
+//     }catch(err){
+//         console.log(err.message);
+//     }
+// })
+
+User.sync({force:false})
+.then(cb=>{
+    app.listen(3020);
+})
+.catch(err=>{
+    console.log(err.message);
+})
